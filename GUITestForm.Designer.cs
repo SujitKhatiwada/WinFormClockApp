3a0e696138dc760df4b0cc1319af4daf7dd8056a
Sujit Khatiwada <98233580+SujitKhatiwada@users.noreply.github.com>
2024-11-22 13:44:26 -0500

    Add files via upload

---
+++ GUITestForm.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace GUITestApplication
+{
+    partial class GUITestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
---
+++ GUITestForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GUITestApplication
+{
+    public partial class GUITestForm : Form
+    {
+        private Label currentTimeLabel = new Label();
+
+        //1: Created a property so code outside this class can read and write to currentTimeLabel.Text
+        public string CurrentTimeText
+        {
+            get { return currentTimeLabel.Text; }
+            set { currentTimeLabel.Text = value; }
+        }
+
+        // A background worker thread to update the label to current time
+        private BackgroundWorker worker = new BackgroundWorker();
+
+        public GUITestForm()
+        {
+            InitializeComponent();
+            SetupForm();
+
+            // Hook to DoWork event and Run the background worker 
+            worker.DoWork += Worker_DoWork;
+            worker.WorkerSupportsCancellation = true;
+            worker.RunWorkerAsync();
+        }
+
+        private void SetupForm()
+        { 
+            //Setup the properties of the display, form, and add label
+            this.Text = "GUI Test Application";
+            this.BackColor = System.Drawing.Color.Gray;
+            this.Size = new System.Drawing.Size(400, 300);
+
+            currentTimeLabel.Font = new System.Drawing.Font("Arial", 24, System.Drawing.FontStyle.Bold);
+            currentTimeLabel.ForeColor = System.Drawing.Color.DarkBlue;
+            currentTimeLabel.AutoSize = true;
+            currentTimeLabel.Location = new System.Drawing.Point((this.Width - currentTimeLabel.Width) / 2, this.Height / 2);
+            this.Controls.Add(currentTimeLabel);
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Keep the worker running until cancellation is requested
+            while (!worker.CancellationPending)
+            {
+                try
+                {
+                    //#2: Got the current time and set currentTimeLabel.Text
+                    // Updating the label on UI thread using Invoke to safely access UI controls from the worker thread
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        CurrentTimeText = DateTime.Now.ToString("hh:mm:ss tt");
+                    }));
+
+                    // Sleep for 1 second before updating the time again
+                    Thread.Sleep(1000);
+                }
+                catch (ObjectDisposedException)
+                {
+                    // If the worker or the form is disposed, exit the loop gracefully to stop the worker
+                    break;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (worker.IsBusy)
+                {
+                    worker.CancelAsync();
+                    Thread.Sleep(500);
+                }
+                worker.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
